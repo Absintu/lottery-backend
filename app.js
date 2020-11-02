d9e19cf295f19b456cc5b700f2bc10b089d22d74
Absintu <joaosf7@gmail.com>
2020-11-02 07:26:53 +0000

    Three timed lotterys simult.

--- app.js
+++ app.js
@@ -1,99 +1,26 @@
 const express = require('express')
 const {Worker, isMainThread, parentPort} = require('worker_threads')
-if(isMainThread){
-    // create the worker
-    const worker = new Worker(__filename)
-    // Listen for messages from the worker and print them
-    worker.on('message', (msg) => {
-        console.log(msg)
-    })
-    
-    const app = express()
-    const PORT = process.env.port || 3000
-    app.get("/", (req, res)=>{
-        res.send('Lottery Logger')
-    })
+// create the worker
+const dailySlave = new Worker('./components/dailySlave.js')
+const weeklySlave = new Worker('./components/weeklySlave.js')
+const monthlySlave = new Worker('./components/monthlySlave.js')
+// Listen for messages from the worker and print them
+dailySlave.on('message', (msg) => {
+    console.log('DailyLottery msg: ' + msg)
+})
 
-    app.listen(PORT, () => console.log('Listening on port' + PORT))
-} else{
-    const fs = require('fs')
-    const Web3 = require ('web3')                                                                                       
-    const address = '0xEA8aCa6C0712d0b23fdFeAAf4ae88479fBfa6756'
-    const Lottery = require ('./Lottery.json')
-    const privateKey = fs.readFileSync('./pkey.txt', 'utf8')
-    const infuraUrl = 'https://goerli.infura.io/v3/71a59d0af9144e5aaf47e0a37c631f2c'
-    // LOTTERY_MINUTES -> Countdown to pick a Winner
-    const LOTTERY_MINUTES = 1
-    
-    // Initializes web3 and calls the pickWinner() method from the Lottery contract
-    const resetLottery = async () =>{
-        const web3 = new Web3(infuraUrl)
-        const networkId =  await web3.eth.net.getId()
-        parentPort.postMessage('NetworkId: ' + networkId)
-        parentPort.postMessage('Contract Address: ' + Lottery.networks[networkId].address)
-        const lottery = new web3.eth.Contract(
-            Lottery.abi,
-            Lottery.networks[networkId].address
-        )
-        const players = await lottery.methods.getPlayers().call()
-        parentPort.postMessage('length: ' + players.length)
-        if(players.length > 0){
-            parentPort.postMessage(await lottery.methods.getPlayers().call())
-            const tx = lottery.methods.pickWinner();
-            parentPort.postMessage('address: ' + address)
-            const data = tx.encodeABI();
-            const gas = await web3.eth.estimateGas({
-                    from: address,
-                    data,
-                    to: lottery.options.address
-            });
-            const gasPrice = await web3.eth.getGasPrice();
-            const nonce = await web3.eth.getTransactionCount(address)
-            parentPort.postMessage('lottery.options.address: ' + lottery.options.address)
-            parentPort.postMessage('data: ' + data)
-            parentPort.postMessage('Gas: ' + gas)
-            parentPort.postMessage('Gas Price: ' + gasPrice)
-            parentPort.postMessage('Nonce: ' + nonce)
-            parentPort.postMessage('ChainId: ' + networkId)
-            parentPort.postMessage('PrivateKey:......')
-            parentPort.postMessage('Trying to send Transaction...') 
-            const signedTx = await web3.eth.accounts.signTransaction(
-                {
-                    to: lottery.options.address,
-                    data,
-                    gas,
-                    gasPrice,
-                    nonce,
-                    chainId: networkId
-                },
-                privateKey
-            );
-            const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
-            parentPort.postMessage('Transaction hash: ' + receipt.transactionHash)
-          } else{
-            parentPort.postMessage('No players on this round. Restarting Lottery...')
-        }
+weeklySlave.on('message', (msg) => {
+    console.log('WeeklyLottery msg: ' + msg)
+})
 
-    }
+monthlySlave.on('message', (msg) => {
+    console.log('MonthlyLottery msg: ' + msg)
+})
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}    
-    // Starts the counter in minutes and calls the resetLottery() method
-    (async ()=>{
-        while(true){
-            parentPort.postMessage('******************* Picking a Winner and reseting the Lottery contract **********')
-            parentPort.postMessage(new Date())
-            try{
-                await resetLottery();
-            }catch(err){
-                parentPort.postMessage('No players or transaction error: ' + err)
-            }
-            parentPort.postMessage('************ Starting new Countdown *********')
-                await sleep(LOTTERY_MINUTES * 60 * 1000)
-    
-            // Start web3 and send transaction to the Lottery contract
-    
-        }
-    })()
-}
+const app = express()
+const PORT = process.env.port || 3000
+app.get("/", (req, res)=>{
+    res.send('Lottery Logger')
+})
+
+app.listen(PORT, () => console.log('Listening on port' + PORT))
---
+++ components/monthlySlave.js
@@ -0,0 +1,101 @@
+const fs = require('fs')
+const Web3 = require ('web3')                                                                                       
+const address = '0xEA8aCa6C0712d0b23fdFeAAf4ae88479fBfa6756'
+const Lottery = require ('./LotteryEthMonthly.json')
+const privateKey = fs.readFileSync('./components/pkey.txt', 'utf8')
+const infuraUrl = 'https://goerli.infura.io/v3/71a59d0af9144e5aaf47e0a37c631f2c'
+const {parentPort} = require('worker_threads')
+// LOTTERY_MINUTES -> Countdown to pick a Winner
+const LOTTERY_MINUTES = 1
+
+// Initializes web3 and calls the pickWinner() method from the Lottery contract
+const resetLottery = async () =>{
+    const web3 = new Web3(infuraUrl)
+    const networkId =  await web3.eth.net.getId()
+    parentPort.postMessage('NetworkId: ' + networkId)
+    parentPort.postMessage('Contract Address: ' + Lottery.networks[networkId].address)
+    const lottery = new web3.eth.Contract(
+        Lottery.abi,
+        Lottery.networks[networkId].address
+    )
+    const players = await lottery.methods.getPlayers().call()
+    parentPort.postMessage('length: ' + players.length)
+    if(players.length > 0){
+        parentPort.postMessage(await lottery.methods.getPlayers().call())
+        const tx = lottery.methods.pickWinner();
+        parentPort.postMessage('address: ' + address)
+        const data = tx.encodeABI();
+        const gas = await web3.eth.estimateGas({
+                from: address,
+                data,
+                to: lottery.options.address
+        });
+        const gasPrice = await web3.eth.getGasPrice();
+        const nonce = await web3.eth.getTransactionCount(address)
+        parentPort.postMessage('lottery.options.address: ' + lottery.options.address)
+        parentPort.postMessage('data: ' + data)
+        parentPort.postMessage('Gas: ' + gas)
+        parentPort.postMessage('Gas Price: ' + gasPrice)
+        parentPort.postMessage('Nonce: ' + nonce)
+        parentPort.postMessage('ChainId: ' + networkId)
+        parentPort.postMessage('PrivateKey:......')
+        parentPort.postMessage('Trying to send Transaction...') 
+        const signedTx = await web3.eth.accounts.signTransaction(
+            {
+                to: lottery.options.address,
+                data,
+                gas,
+                gasPrice,
+                nonce,
+                chainId: networkId
+            },
+            privateKey
+        );
+        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+        parentPort.postMessage('Transaction hash: ' + receipt.transactionHash)
+        } else{
+        parentPort.postMessage('No players on this round. Restarting Lottery...')
+    }
+
+}
+
+function sleep(ms) {
+return new Promise(resolve => setTimeout(resolve, ms));
+}    
+// Starts the counter in minutes and calls the resetLottery() method
+(async ()=>{
+    while(true){
+        parentPort.postMessage('******************* Picking a Winner and reseting the Lottery contract **********')
+        parentPort.postMessage(new Date())
+        try{
+            await resetLottery();
+        }catch(err){
+            parentPort.postMessage('No players or transaction error: ' + err)
+        }
+        parentPort.postMessage('************ Starting new Countdown *********')
+        let date = new Date()
+        let oldDate = date.getDate()
+        const currMonth = date.getMonth()
+        let finalDay
+        if (currMonth == 2)
+            finalDay = 28
+        else if (currMonth == 1 || currMonth == 3 || currMonth == 5 || currMonth == 7 ||
+                currMonth == 8 || currMonth == 10 || currMonth == 12)
+            finaDay = 31
+        else
+            finalDay = 30
+        while(date.getDate() != finalDay){
+            parentPort.postMessage(date)
+            parentPort.postMessage('Im the Monthly Slave and im alive :)')
+            await sleep(1000 * 60 * 60 * 3)
+            date = new Date()
+        }
+        while(date.getDate() > 1 ){
+            parentPort.postMessage(date)
+            parentPort.postMessage('Im the Monthly Slave and im alive :)')
+            await sleep(60 * 1000)
+            date = new Date()
+        }
+        // Start web3 and send transaction to the Lottery contract
+    }
+})()
